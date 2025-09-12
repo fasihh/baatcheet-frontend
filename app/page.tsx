9c6c3bb040e980fbd7dd68f79b39e42afbe4fb45
fasihh <fhk.tcs@gmail.com>
2025-09-12 08:24:32 +0500

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,10 @@
-"use client"
-
-import { useState } from "react";
-import { useSocket } from "@/context/socket-provider";
+"use client";
 
 export default function Page() {
-  const { sendMessage, messages } = useSocket();
-  const [message, setMessage] = useState('');
-
   return (
-    <div>
-      <div>
-        <h1>All messages will appear here</h1>
-      </div>
-      <div>
-        <input onChange={e => setMessage(e.target.value)} className="w-24 h-12 p-2.5 border-[1px] border-solid" placeholder="Message..." />
-        <button onClick={e => sendMessage(message)} className="h-12 w-12 p-2.5">Send</button>
-      </div>
-      <div>
-        {messages.map((e, idx) => (
-          <li key={idx}>{e}</li>
-        ))}
+    <div className="flex justify-center items-center bg-background h-screen">
+      <div className="text-4xl font-semibold">
+        Hello World!
       </div>
     </div>
   );
