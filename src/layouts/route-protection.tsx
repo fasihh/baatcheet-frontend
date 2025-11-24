d50eb4965884aa2b62432b8910b23c4a16288a0c
fasihh <fhk.tcs@gmail.com>
2025-11-25 00:39:49 +0500

    refactor: improve route protection with custom predicate

--- src/layouts/route-protection.tsx
+++ src/layouts/route-protection.tsx
@@ -2,13 +2,17 @@ import { useUser } from "@/contexts/user";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function RouteProtection({ children }: { children: React.ReactNode }) {
+function RouteProtection({
+  children,
+  redirect,
+  predicate = (token) => !token
+}: { children: React.ReactNode, redirect?: string, predicate?: (token?: string | null) => boolean }) {
   const navigate = useNavigate();
   const { token } = useUser();
 
   React.useEffect(() => {
-    if (!token)
-      navigate("/login");
+    if (predicate(token))
+      navigate(redirect || "/login");
   }, [token]);
 
   return children;
