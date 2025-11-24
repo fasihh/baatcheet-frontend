41ee098d3959a430493e17909dd92499b01f7484
fasihh <fhk.tcs@gmail.com>
2025-11-25 00:35:49 +0500

    feat: add more routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,35 @@ import RouteProtection from './layouts/route-protection';
 import ChatPage from './pages/chat-room';
 import FriendRequestPage from './pages/me';
 import GuildPage from './pages/guild';
+import { Suspense } from 'react';
+import { Toaster } from '@/components/ui/sonner';
 
 const NotFound = () => (
-    <Empty>
-      <EmptyHeader>
-        <EmptyTitle>404 - Not Found</EmptyTitle>
-        <EmptyDescription>
-          The page you&apos;re looking for doesn&apos;t exist. Try searching for
-          what you need below.
-        </EmptyDescription>
-      </EmptyHeader>
-    </Empty>
-  );
+  <Empty>
+    <EmptyHeader>
+      <EmptyTitle>404 - Not Found</EmptyTitle>
+      <EmptyDescription>
+        The page you&apos;re looking for doesn&apos;t exist. Try searching for
+        what you need below.
+      </EmptyDescription>
+    </EmptyHeader>
+  </Empty>
+);
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<LoginPage isRegister />} />
+        <Route path="/login" element={
+          <RouteProtection predicate={(token) => !!token} redirect="/me">
+            <LoginPage />
+          </RouteProtection>
+        } />
+        <Route path="/register" element={
+          <RouteProtection predicate={(token) => !!token} redirect="/me">
+            <LoginPage isRegister />
+          </RouteProtection>
+        } />
         <Route
           element={
             <RouteProtection>
@@ -34,13 +44,18 @@ function App() {
         >
           <Route path="/me" element={<FriendRequestPage />} />
           <Route path="/chats/:chatId" element={<ChatPage />} />
-          <Route path="/guilds/:guildId" element={<GuildPage />}>
+          <Route path="/guilds/:guildId" element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <GuildPage />
+            </Suspense>
+          }>
             <Route path="chats/:chatId" element={<ChatPage />} />
           </Route>
         </Route>
         <Route path="/" element={<Navigate to="/me" replace />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Toaster />
     </Router>
   );
 }
