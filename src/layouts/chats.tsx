124ca52318d8e92c13384465240c4e99a377a333
fasihh <fhk.tcs@gmail.com>
2025-11-25 00:39:32 +0500

    refactor: adjust for permissions when fetching for gulds

--- src/layouts/chats.tsx
+++ src/layouts/chats.tsx
@@ -3,12 +3,33 @@ import { FriendsChatPanel } from '@/components/friends/friends-chat-panel';
 import ErrorBoundary from '@/components/error-boundary';
 import { GuildsList } from '@/components/guilds/guilds-list';
 import { Suspense } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import { GuildChatsPanel } from '@/components/guilds/guild-chats-panel';
+import { GuildPermissionWrapper } from '@/components/guilds/guild-permission-wrapper';
+import { UserProfile } from '@/components/user-profile';
 
 function ChatLayout() {
   const location = useLocation();
-  const isGuildRoute = location.pathname.startsWith('/guilds/');
+  const { guildId } = useParams();
+  const isGuildRoute = !!guildId;
+
+  const content = (
+    <ErrorBoundary resetKeys={[location.pathname]}>
+      {/* Middle panel: Friends / DMs or Guild Channels */}
+      <Card className="min-w-[16rem] h-full flex flex-col pb-0">
+        {isGuildRoute ? <GuildChatsPanel /> : <FriendsChatPanel />}
+        <UserProfile />
+      </Card>
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">
+        {/* Page Content */}
+        <div className="flex-1 overflow-y-auto">
+          <Outlet />
+        </div>
+      </div>
+    </ErrorBoundary >
+  );
 
   return (
     <div className="flex h-screen">
@@ -25,20 +46,19 @@ function ChatLayout() {
         <GuildsList />
       </Suspense>
 
-      <ErrorBoundary resetKeys={[location.pathname]}>
-        {/* Middle panel: Friends / DMs or Guild Channels */}
-        <Card className="min-w-[16rem] h-full flex flex-col">
-          {isGuildRoute ? <GuildChatsPanel /> : <FriendsChatPanel />}
-        </Card>
-
-        {/* Main Content */}
-        <div className="flex-1 flex flex-col">
-          {/* Page Content */}
-          <div className="flex-1 overflow-y-auto">
-            <Outlet />
+      {guildId ? (
+        <Suspense fallback={
+          <div className="flex-1 flex items-center justify-center">
+            Loading guild...
           </div>
-        </div>
-      </ErrorBoundary >
+        }>
+          <GuildPermissionWrapper guildId={guildId}>
+            {content}
+          </GuildPermissionWrapper>
+        </Suspense>
+      ) : (
+        content
+      )}
     </div >
   );
 };
