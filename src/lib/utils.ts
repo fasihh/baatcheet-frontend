f2b232ea56156ae6e756b99f8a1af00c2bd142da
fasihh <fhk.tcs@gmail.com>
2025-11-24 16:33:42 +0500

    feat: add fetcher wrapper for fetch

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function fetcher<T>(method: string, url: string, token?: string, options?: RequestInit): Promise<T> {
+  return fetch(url, {
+    method,
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      ...(options?.headers || {}),
+    },
+  }).then((res) => {
+    if (!res.ok) {
+      return res.json().then((data) => {
+        throw new Error(data.error?.info || 'An error occurred while fetching data');
+      });
+    }
+    return res.json();
+  });
+}
