f166273046974a79048261828345046115f9ad02
fasihh <fhk.tcs@gmail.com>
2025-11-25 20:06:59 +0500

    fix!: use navigate directly to redirect based on predicate instead of within useEffect

--- src/layouts/route-protection.tsx
+++ src/layouts/route-protection.tsx
@@ -1,19 +1,16 @@
 import { useUser } from "@/contexts/user";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function RouteProtection({
   children,
   redirect,
   predicate = (token) => !token
 }: { children: React.ReactNode, redirect?: string, predicate?: (token?: string | null) => boolean }) {
-  const navigate = useNavigate();
   const { token } = useUser();
 
-  React.useEffect(() => {
-    if (predicate(token))
-      navigate(redirect || "/login");
-  }, [token]);
+  if (predicate(token))
+    return <Navigate to={redirect || "/login"} />;
 
   return children;
 }
