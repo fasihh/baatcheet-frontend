c3aac8d98a19c4e1a70759cb73c47831d7b331ee
fasihh <fhk.tcs@gmail.com>
2025-11-26 22:20:57 +0500

    feat: add hide flag

--- src/constants/permissions.ts
+++ src/constants/permissions.ts
@@ -1,9 +1,9 @@
 export const PERMISSIONS = [
-  { key: 'can_manage_channels', label: 'Manage Channels' },
-  { key: 'can_manage_roles', label: 'Manage Roles' },
-  { key: 'can_manage_permissions', label: 'Manage Permissions' },
-  { key: 'can_kick_members', label: 'Kick Members' },
-  { key: 'can_ban_members', label: 'Ban Members' },
-  { key: 'can_message', label: 'Send Messages' },
-  { key: 'can_change_owner', label: 'Change Owner' },
+  { key: 'can_manage_channels', label: 'Manage Channels', hide: false },
+  { key: 'can_manage_roles', label: 'Manage Roles', hide: false },
+  { key: 'can_manage_permissions', label: 'Manage Permissions', hide: false },
+  { key: 'can_kick_members', label: 'Kick Members', hide: false },
+  { key: 'can_ban_members', label: 'Ban Members', hide: false },
+  { key: 'can_message', label: 'Send Messages', hide: true },
+  { key: 'can_change_owner', label: 'Change Owner', hide: true },
 ];
